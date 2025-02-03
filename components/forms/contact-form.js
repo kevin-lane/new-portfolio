de748e8ef8bc597b19f5de1e5ad5a0ff4c3481f0
Kevin <kevinlane3@hotmail.com>
2025-02-03 17:21:24 +0100

    Contact form is working now

--- components/forms/contact-form.js
+++ components/forms/contact-form.js
@@ -1,29 +1,21 @@
-'use client';
-
 export default function ContactForm() {
-  const handleSubmit = (e) => {
-    e.preventDefault();
-
-    alert('Sorry, this feature is currently under construction. Please send me an email instead and I will get back to you as soon as I can!');
-  }
-
   return (
-    <form onSubmit={handleSubmit}>
+    <form action={`https://formspree.io/f/${process.env.FORMSPREE_FORM_ID}`} method="POST">
       <div className='mb-5'>
         <label className='ml-8 max-[400px]:ml-12' htmlFor="name">Name</label><br/>
-        <input className='w-80 p-3 border border-indigo-300 rounded mt-2 ml-8 max-[400px]:ml-12' type="text" id='name' name="name"  placeholder='Name' required/><br/>
+        <input className='w-80 p-3 border border-indigo-300 rounded mt-2 ml-8 text-black max-[400px]:ml-12' type="text" id='name' name="name"  placeholder='Name' required/><br/>
       </div>
 
       <div className='mb-5'>
         <label className='ml-8 max-[400px]:ml-12' htmlFor="email">Email</label><br/>
-        <input className='w-80 p-3 border border-indigo-300 rounded mt-2 ml-8 max-[400px]:ml-12' type="email" name="email" placeholder='Email' required/><br/>
+        <input className='w-80 p-3 border border-indigo-300 rounded mt-2 ml-8 text-black max-[400px]:ml-12' type="email" name="email" placeholder='Email' required/><br/>
       </div>
 
       <div className='mb-5'>
         <label className='ml-8 max-[400px]:ml-12' htmlFor="message">Message</label><br/>
-        <textarea className='w-80 p-3 h-32 border border-indigo-300 rounded mt-2 ml-8 max-[400px]:ml-12' id='message' name='message' placeholder='Your message' required></textarea><br/>
+        <textarea className='w-80 p-3 h-32 border border-indigo-300 rounded mt-2 ml-8 text-black max-[400px]:ml-12' id='message' name='message' placeholder='Your message' required></textarea><br/>
       </div>
       <button className='w-40 h-12 ml-8 mb-8 max-[400px]:ml-12 bg-blue-700 text-white font-semibold rounded' type="submit">Send</button>
     </form>
-  )
+  );
 }
