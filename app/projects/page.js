8e06df6e8baf027dc6ef63ff06674506aea6d6d4
Kevin <kevinlane3@hotmail.com>
2025-06-08 19:41:41 +0200

    Some code clean up and us ProjectsPage() instead

--- app/projects/page.js
+++ app/projects/page.js
@@ -1,8 +1,6 @@
 import ProjectCard from '@/components/cards/project-card';
 import Link from 'next/link';
 import { getProjects } from '@/lib/projects';
-import kelanPhoto from '@/assets/kelanphotography.jpg';
-import weatherVue from '@/assets/weather-vue.jpg';
 
 //Meta data for SEO and web availability
 export const metadata = {
@@ -10,26 +8,15 @@ export const metadata = {
   description: "Check out my portfolio and projects. I would love to help you with your web project as well!",
 };
 
-export default async function Projects() {
+export default async function ProjectsPage() {
   const projects = await getProjects();
-  const updatedProjects = projects.map((project) => ({
-    ...project,
-    image: project.slug === 'kelan-photography' ? kelanPhoto :
-           project.slug === 'weather-vue' ? weatherVue :
-           project.image || null,  // Fallback if you add more projects
-  }));
-
-  console.log(projects);
-
-  console.log(updatedProjects);
-
   return (
     <section className='min-h-screen flex items-center justify-center ml-6'>
       <article>
         <h1 className="font-black mt-32 mb-4 text-3xl w-96 max-lg:mt-16 ml-12">PROJECTS</h1>
         <p className="mb-8 ml-12 w-96">Here you will find some of my projects, both school and hobby projects. </p>
 
-        {updatedProjects.map((project) =>{
+        {projects.map((project) =>{
           console.log(project);
 
           return(
